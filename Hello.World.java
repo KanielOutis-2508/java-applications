b7203a836fbf6e56516e16b58eb5489a0228893a
KanielOutis-2508! <omiunuagapecarey@gmail.com>
2025-10-23 11:02:57 +0100

    first java program

---
+++ Hello.World.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main (string[] agrgs){
+		System.out.println("Hello World, This is my First java Program");
+		System.out.println('Learning java is fun');
+	}
+	
+}	
